3ba60b7ac20c271bf57373abd3a981f12c0b9e06
Jhosselin Iriarte <jhoseliniriarte1@gmail.com>
2024-12-11 20:21:41 -0400

    Editado de vistas

--- resources/views/unidades/index.blade.php
+++ resources/views/unidades/index.blade.php
@@ -44,12 +44,14 @@
                                 {{$unidad->descripcion}}
                             </td>
                             <td class="px-6 py-4 text-right">
-                                <a href="{{route('unidades.edit', $unidad)}}"
-                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
+                                <a href="{{route('unidades.edit', $unidad)}}
                                 <form action="{{route('unidades.destroy', $unidad)}}" method="POST">
+                                <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Editar</button> 
                                     @method('DELETE')
-                                    <button class="font-medium text-blue-600 dark:text-red-500 hover:underline">Eliminar</>
-                                        @csrf
+                                    
+                                    <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Eliminar</button>
+                                       
+                                    @csrf
                                 </form>
                             </td>
                         </tr>
